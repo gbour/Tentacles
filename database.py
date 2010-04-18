9c323c7ba0a57ed01a71dbca8770932a95460b85
Guillaume Bour <guillaume@bour.cc>
2010-04-18 22:55:16 +0000

    start of cleaning
    git-svn-id: http://devedge.bour.cc/svn/tentacles/trunk@19 ff29af12-de15-47db-b328-d843b730dfdc

--- database.py
+++ database.py
@@ -2,44 +2,52 @@
 import sys, re, inspect, types
 
 class Uri(object):
-    def __init__(self, raw):
-        m = re.match('^(?P<scheme>\w+):(//((?P<username>[\w]+):(?P<password>[\w]+)@)?(?P<host>[\w.]+)(?P<port>\d+)?/)?(?P<db>[\w/.]+)', raw, re.U|re.L)
-    
-        for k, v in m.groupdict().iteritems():
-            setattr(self, k, v)
+	def __init__(self, raw):
+		m = re.match('^(?P<scheme>\w+):(//((?P<username>[\w]+):(?P<password>[\w]+)@)?(?P<host>[\w.]+)(?P<port>\d+)?/)?(?P<db>[\w/.]+)', raw, re.U|re.L)
+	
+		for k, v in m.groupdict().iteritems():
+			setattr(self, k, v)
 
 class Database(object):
-    __tables__ = {}
+	__instance__  = None
+	__tables__    = {}
 
-    def __init__(self, uri, setglobal=True):
-        self.uri = Uri(uri)
-        
-        modname = "%s.backends.%s" % (self.__module__.rsplit('.', 1)[0], self.uri.scheme)
-        if not sys.modules.has_key(modname):
-            raise Exception("Unknown '%s' database backend" % uri.scheme)
-        
-        exec "from %s import *" % modname
-        sdb = SQLiteDatabase(self.uri)
-        
-        for name, obj in inspect.getmembers(sdb):
-            if name.startswith('__'):
-                continue
-                
-            if isinstance(obj, types.MethodType):
-                obj = types.MethodType(obj.im_func, self)
-            setattr(self, name, obj)
-            
-        if setglobal:
-            sys.modules[self.__module__.split('.', 1)[0]].__DATABASE__ = self
-        
-    def toto(self):
-        print "Database::toto"
-        
-        
-    @staticmethod
-    def register_table(table):
-        Database.__tables__[table.__name__] = table
+	def __init__(self, uri):
+		self.uri = Uri(uri)
+		
+		modname = "tentacles.backends.%s" % self.uri.scheme
+		if not sys.modules.has_key(modname):
+			raise Exception("Unknown '%s' database backend" % self.uri.scheme)
+		
+		exec "from %s import *" % modname
+		if not hasattr(sys.modules[modname], 'Database'):
+			raise Exception("Unknown '%s' database backend" % self.uri.scheme)
 
-    def create_tables(self):
-        for table in Database.__tables__.itervalues():
-            print table
+		backend = getattr(sys.modules[modname], 'Database')
+		for name, obj in inspect.getmembers(backend):
+			if name.startswith('__') or hasattr(self, name):
+				continue
+				
+			if isinstance(obj, types.MethodType):
+				obj = types.MethodType(obj.im_func, self)
+			setattr(self, name, obj)
+		
+		for table in self.__tables__.itervalues():
+			table.__inherit__(self)
+
+		self.__class__.__instance__ = self
+		
+	@classmethod
+	def register_table(cls, table):
+		"""Register tables for this database.
+
+			Tables register once themselves at definition time (metaclass.__init__)
+		"""
+		cls.__tables__[table.__table_name__] = table
+
+		if cls.__instance__:
+			table.__inherit__(cls.__instance__)
+
+	def create_tables(self):
+		for table in Database.__tables__.itervalues():
+			table.create()
