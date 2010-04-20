e482277583ddb7c804211cbe6ab8e47cf1e1a498
guillaume <guillaume@ff29af12-de15-47db-b328-d843b730dfdc>
2010-04-20 16:41:57 +0000

    . more more cleaning . manage table reference fields (one 2 many, many 2 many)
    git-svn-id: http://devedge.bour.cc/svn/tentacles/trunk@22 ff29af12-de15-47db-b328-d843b730dfdc

--- fields.py
+++ fields.py
@@ -1,28 +1,23 @@
 # -*- coding: utf8 -*-
 
 import inspect, types, sys
+from tentacles import Database
 
+ORDER = 0
 
-#ORDER = 0
-#class FieldDescription(object):
-#    def __init__(self, klass, args, kwargs):
-#        global ORDER 
-#        self.order  = ORDER
-#        ORDER += 1
-#        
-#        self.klass  = klass
-#        self.args   = args
-#        self.kwargs = kwargs
+class MetaField(type):
+	def __new__(cls, name, bases, dct):
+		global ORDER
+		dct['__order__'] = ORDER
+		ORDER += 1
 
-#class ReferenceDescription(FieldDescription):
-        
-#        print dir(sys.modules['__main__'])
-#        print sys.modules.keys()
-    
+		return type.__new__(cls, name, bases, dct)
 
 class Field(object):
-    @classmethod
-    def __inherit__(cls, database):
+	__metaclass__ = MetaField
+
+	@classmethod
+	def __inherit__(cls, database):
 		modname = "tentacles.backends.%s.fields" % database.uri.scheme
 		exec "import %s" % modname
 		backend = getattr(sys.modules[modname], cls.__name__)
@@ -32,99 +27,58 @@ class Field(object):
 				continue
 
 			if isinstance(obj, types.MethodType):
-				if obj.im_self is not None:                       # class method
+				if obj.im_self is not None:																# class method
 					obj = types.MethodType(obj.im_func, cls)
 				else:
 					obj = obj.im_func
 			
 			setattr(cls, name, obj)
 
-#    type = types.NoneType
-#    isfield = True
-
-#    def __new__(cls, *args, **kwargs):
-#        # Table fields must be instanciated only after table is instanciated
-#        # (so take the correct database value)
-#        if 'database' not in kwargs:
-#            if issubclass(cls, Reference):
-#                 kwargs['remote'] = args[0]
-#                 args = ()
-##                return ReferenceDescription(cls, args, kwargs)
-#            return FieldDescription(cls, args, kwargs)
-#            
-#        inst = super(Field, cls).__new__(cls, *args, **kwargs)
-#        db = kwargs['database']
-#        modname = "orm.backends.%s" % db.uri.scheme
-#        exec "from %s import *" % modname
-
-#        dbfield = getattr(sys.modules[modname], cls.__name__)
-##        print 'dbfield=', dbfield
-
-#        for name, obj in inspect.getmembers(dbfield):
-#            if name.startswith('__'):
-#                continue
-
-#            if isinstance(obj, types.MethodType):
-#                obj = types.MethodType(obj.im_func, inst)
-
-#            setattr(inst, name, obj)
-
-#        return inst
-
-#    def __init__(self, name=None, notnull=False, primary_key=False, database=None, order=-1, **kwargs):
-#        self.name       = name
-#        self.notnull    = notnull
-#        self.pk         = primary_key
-#        self.order      = order
-#        self.unique     = False
-#        
-#        if 'default' in kwargs:
-#            self.default   = kwargs['default']
-#        if 'remote' in kwargs:
-#            self.remote    = kwargs['remote']
-#        if 'owner' in kwargs:
-#            self.owner    = kwargs['owner']
-
-#        self.database   = database if database else orm.__DATABASE__
-#        
-#        modname = "%s.backends.%s" % (self.__module__.split('.', 1)[0], self.database.uri.scheme)
-#        exec "from %s import *" % modname
-
-#        try:
-#            dbfield = eval("%s.%s()" % (modname, self.__class__.__name__))
-#        except AttributeError:
-#            raise Exception("*%s* type not defined in *%s* backend" % \
-#                (self.__class__.__name__, self.database.uri.scheme))
-
-#        for name, obj in inspect.getmembers(dbfield):
-#            if name.startswith('__'):
-#                continue
-
-#            if isinstance(obj, types.MethodType):
-#                obj = types.MethodType(obj.im_func, self)
-#                
-#            setattr(self, name, obj)
+	def __init__(self, name=None, notnull=False, primary_key=False, **kwargs):
+		self.name        = name
+		self.notnull     = notnull
+		self.pk          = primary_key
+		self.unique      = False
+		self.__hidden__  = False
+		
+		if 'default' in kwargs:
+			self.default   = kwargs['default']
+		if 'unique' in kwargs:
+			self.unique    = kwargs['unique']
+#		if 'remote' in kwargs:
+#			self.remote	= kwargs['remote']
+#		if 'owner' in kwargs:
+#			self.owner	= kwargs['owner']
 
 
 class Integer(Field):
-#    type = int
-    pass
+#	type = int
+	pass
 
 class String(Field):
-#    type = unicode
-    pass
+#	type = unicode
+	pass
 
 class Binary(Field):
-    pass
-    
+	pass
+	
 class Boolean(Field):
-    pass
-    
+	pass
+	
 class Datetime(Field):
-    pass
+	pass
 
 class Reference(Field):
-    pass
-    
-class ReferenceSet(Reference):
-    pass
+	def __init__(self, remote, **kwargs):
+		super(Reference, self).__init__(**kwargs)
+		self.remote = remote
+	
+class ReferenceSet(Field):
+	def __init__(self, remote, *args, **kwargs):
+		super(ReferenceSet, self).__init__(*args, **kwargs)
+		
+		self.remote     = remote
+		self.__hidden__ = True
+		
+		Database.register_reference(self)
+
