2cdc4b5f510b5ac392d07f811d70b74bb7e793f7
Guillaume Bour <guillaume@bour.cc>
2010-05-31 20:01:16 +0000

    add licence headers
    git-svn-id: http://devedge.bour.cc/svn/tentacles/trunk@58 ff29af12-de15-47db-b328-d843b730dfdc

--- tentacles/table.py
+++ tentacles/table.py
@@ -1,4 +1,25 @@
 # -*- coding: utf8 -*-
+"""
+    tentacle, python ORM
+    Copyright (C) 2010	Guillaume Bour <guillaume@bour.cc>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+__author__  = "Guillaume Bour <guillaume@bour.cc>"
+__version__ = "$Revision$"
+__date__    = "$Date$"
+
 import sys, types, inspect, weakref
 from odict import odict
 
