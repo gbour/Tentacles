2b87876d84a03c782e664d0880bdf8187410ab91
Guillaume Bour <guillaume@bour.cc>
2011-04-02 21:50:22 +0000

    [doc] add long_description + doc files in setup.py
    git-svn-id: http://devedge.bour.cc/svn/tentacles/trunk@185 ff29af12-de15-47db-b328-d843b730dfdc

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ from distutils.core import setup
 setup(
 	name         = 'tentacles',
 	version      = '0.1.0',
-	description  = 'ORM',
+	description  = 'Object-Relational Model (ORM)',
 	author       = 'Guillaume Bour',
 	author_email = 'guillaume@bour.cc',
-	url          = 'http://devedge.bour.cc/wiki/Tentacles/',
+	url          = 'http://devedge.bour.cc/wiki/Tentacles',
 	license      = 'GNU Affero General Public License v3',
 	classifiers  = [
 		'Development Status :: 3 - Alpha',
@@ -36,7 +36,52 @@ setup(
 		'Topic :: Software Development :: Libraries :: Python Modules',
 	],
 
-	long_description = """""",
+	long_description = """Tentacles is a python ORM.
+	The main idea is to manipulate stored datas as you do for python data structures
+
+	python native	
+	>>> class SuperHero(object):
+	>>>		def __init__(self, name, gender, power):
+	>>>			self.name   = name
+	>>>			self.gender = gender
+	>>>			self.power  = power
+	>>>
+	>>> hero1 = SuperHero('superman', 'male', 'flight')
+	>>> print "%s can %s" % (hero1.name, hero1.power)
+
+	... vs ...
+
+	tentacles
+	>>> from tentacles import Object, fields
+	>>> class SuperHero(Object):
+	>>>		name   = String()
+	>>> 	gender = String()
+	>>> 	power  = String()
+	>>>
+	>>> hero1 = SuperHero(name='superman', gender='male', power='flight')
+	>>> print "%s can %s" % (hero1.name, hero1.power)
+
+
+	python native
+	>>> heros   = [hero1, SuperHero(name='wonder woman', gender='female', power='enhanced vision')]
+	>>> females = filter(lambda e: e.gender = 'female', heros)
+	>>> for e in females:
+	>>> 	print "superheroine: %s" % e.name
+
+	... vs ...
+
+	tentacles
+	>>> heros   = [hero1, SuperHero(name='wonder woman', gender='female', power='enhanced vision')]
+	>>> females = filter(lambda e: e.gender = 'female', heros)
+	>>> for e in females:
+	>>> 	print "superheroine: %s" % e.name
+
+	Tentacles is pretty yound and incomplete, and still in alpha stage.
+	It currently support only sqlite3 backend, while more are scheduled at mid-term (mysql, postgresql, but also no-sql storages, like openldap, mongodb, ...)
+	""",
+
+
 	packages=['tentacles', 'tentacles.backends.sqlite3'],
+	data_files=[('share/doc/python-tentacles', ('README.md','AUTHORS','COPYING'))],
 	requires=['odict', 'reblok'],
 )
